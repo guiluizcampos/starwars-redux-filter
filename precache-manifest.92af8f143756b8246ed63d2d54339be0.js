a7cbf5932440fab812613b20fe4c5374aad11933
Guilherme Luiz <guiiluiz44@gmail.com>
2020-02-13 21:46:10 -0300

    Updates

---
+++ precache-manifest.92af8f143756b8246ed63d2d54339be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5798588cd0e785bc147df64591b96080",
+    "url": "/guiiluiz/starwars-redux-filter/index.html"
+  },
+  {
+    "revision": "8423bf365184279cc8eb",
+    "url": "/guiiluiz/starwars-redux-filter/static/css/main.c2878e28.chunk.css"
+  },
+  {
+    "revision": "d9684356544cd47fd550",
+    "url": "/guiiluiz/starwars-redux-filter/static/js/2.9a8ced85.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/guiiluiz/starwars-redux-filter/static/js/2.9a8ced85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8423bf365184279cc8eb",
+    "url": "/guiiluiz/starwars-redux-filter/static/js/main.d6da0176.chunk.js"
+  },
+  {
+    "revision": "a1f81c91764730250485",
+    "url": "/guiiluiz/starwars-redux-filter/static/js/runtime-main.a0afbf40.js"
+  }
+]);
